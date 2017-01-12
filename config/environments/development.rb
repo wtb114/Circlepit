052e7c9fb1207dd32d05464da4b28b44668ae0ab
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-12 16:38:06 +0900

    developement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: => 'us-cdbr-iron-east-04.cleardb.net' }
+    config.action_mailer.default_url_options = { host: => 'us-cdbr-iron-east-04.cleardb.net' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

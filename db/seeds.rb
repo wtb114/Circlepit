a870809f567973d110d8e54dc1e44e96b8e8bd05
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-13 16:25:59 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'kengoshidahara114@hotmail.com', password: 'ken114', nickname: 'CirclepitAdmin')

30d8057a38a00d3b32aaaf364bfe11a6767c324e
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-15 19:04:31 +0900

    productiondevise9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.default_url_options = { host: 'circlepit2016@herokuapp.com', port: 3306 }
+config.action_mailer.default_url_options = { protocol: 'https', host: 'circlepit2016@herokuapp.com', port: 3306 }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

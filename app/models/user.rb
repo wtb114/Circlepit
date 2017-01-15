0e2b094bf3b4b9ec399fcea08da90c56ba366278
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-15 22:20:59 +0900

    avator

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,18 +18,18 @@ class User < ActiveRecord::Base
     user
   end
 
-  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user
-      user = User.create(name:     auth.info.nickname,
-                         provider: auth.provider,
-                         uid:      auth.uid,
-                         email:    User.create_unique_email,
-                         password: Devise.friendly_token[0,20]
-                        )
-    end
-    user
-  end
+  # def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  #   unless user
+  #     user = User.create(name:     auth.info.nickname,
+  #                        provider: auth.provider,
+  #                        uid:      auth.uid,
+  #                        email:    User.create_unique_email,
+  #                        password: Devise.friendly_token[0,20]
+  #                       )
+  #   end
+  #   user
+  # end
 
   # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
   def self.create_unique_string
@@ -37,12 +37,12 @@ class User < ActiveRecord::Base
   end
 
   # twitterではemailを取得できないので、適当に一意のemailを生成
-  def self.create_unique_email
-    User.create_unique_string + "@example.com"
-  end
+  # def self.create_unique_email
+  #   User.create_unique_string + "@example.com"
+  # end
 
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  has_attached_file  :avatar, styles:{ medium: "300x300#", thumb: "100x100#"}
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
 

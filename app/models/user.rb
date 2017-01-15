42396d48800c088fbefda6785122c566d9e78bab
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-15 22:33:17 +0900

    avatar4

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
   # end
 
 
-  has_attached_file :avatar, styles: { medium: "300x300!", thumb: "100x100!"}
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
 

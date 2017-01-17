c981b1a64e324b6ff1b20ad3ebb1d77a621448ea
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-17 22:05:54 +0900

    amazons22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,16 +98,16 @@ config.action_mailer.default_url_options = {host: 'https://circlepit2016.herokua
     # :authentication => 'login'
   }
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :bucket => ENV['AMAZON_S3_BUCKET_NAME'],
-  :s3_region => ENV['AMAZON_S3_REGION'],
-  :s3_host_name => ENV['AMAZON_S3_HOST_NAME'],
-  :s3_credentials => {
-    :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
-  }
-}
+# config.paperclip_defaults = {
+#   :storage => :s3,
+#   :bucket => ENV['AMAZON_S3_BUCKET_NAME'],
+#   :s3_region => ENV['AMAZON_S3_REGION'],
+#   :s3_host_name => ENV['AMAZON_S3_HOST_NAME'],
+#   :s3_credentials => {
+#     :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
+#     :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+#   }
+# }
 
 
 end

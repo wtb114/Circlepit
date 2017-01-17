5c9c42a506167d9922688b1a313a00b313123701
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-17 17:23:08 +0900

    dependent2

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,6 +8,6 @@ class Artist < ActiveRecord::Base
   #assosiation
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
-  has_many :events
+  has_many :events, dependent: :destroy
   has_many :groups
 end

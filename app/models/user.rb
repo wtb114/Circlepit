3d37b878c8ab396f499c991e5fbab49563ab6b48
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-17 22:17:24 +0900

    amazons23

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,10 +43,10 @@ class User < ActiveRecord::Base
 
 
   has_attached_file :avatar, styles: { medium: "300x300!", thumb: "100x100!>"},
-    :storage => :s3,
-    :s3_permissions => :private,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
-    :path => ":class/:attachment/:id/:style.:extension"
+    # :storage => :s3,
+    # :s3_permissions => :private,
+    # :s3_credentials => "#{Rails.root}/config/s3.yml",
+    # :path => ":class/:attachment/:id/:style.:extension"
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
 

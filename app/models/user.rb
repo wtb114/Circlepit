41c1637e1c2c43def89fe028e905a0d3fa1d5b0d
志田原健吾 <kengoshidahara@hotmail.com>
2017-01-18 16:47:32 +0900

    s32

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300!", thumb: "100x100!>"},
   :storage => :s3,
+  :s3_permissions => :private,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
   :path => ":attachment/:id/:style.:extension"
 
